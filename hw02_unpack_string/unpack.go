9bb22f09804e648e212fd13632a493d6e0f82754
Feride3D <Feride3d@icloud.com>
2022-02-21 00:38:59 +0300

    Bugs are fixed in HW02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,7 +2,6 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"strconv"
 	"strings"
 	"unicode"
 )
@@ -15,34 +14,35 @@ func Unpack(packedString string) (string, error) {
 	if packedString == "" {
 		return "", nil
 	}
-	// Convert the packedString into a slice of runes.
-	sliceRunes := []rune(packedString)
-	// A new variable of type strings.Builder
-	var unpacked strings.Builder
-	// Search by the slice of runes.
-	for i := range sliceRunes {
-		actualRune := sliceRunes[i]
-		nextRune := sliceRunes[i+1]
+
+	// A new variables with scope in a function.
+	unpacked := strings.Builder{}
+	var actualRune rune
+	isRune := false
+
+	// Search by runes in a string. IsDigit reports whether the rune is a decimal digit.
+	// If an invalid string was passed, the function returns "invalid string".
+	for _, r := range packedString {
 		switch {
-		// If an invalid string was passed (the first rune is a digit), the function returns an error.
-		case (i == 0 && unicode.IsDigit(actualRune)): // IsDigit reports whether the rune is a decimal digit.
-			return "", ErrInvalidString
-		/* If an invalid string was passed (the actual rune is a digit and the next rune
-		(a rune following the actual rune) is a digit), the function returns an error. */
-		case (unicode.IsDigit(actualRune) && unicode.IsDigit(nextRune)):
-			return "", ErrInvalidString
-		}
-		/* If the the next rune (a rune following the actual rune) is a digit,
-		then repeat the actual rune in the amount of the next rune. */
-		if unicode.IsLetter(actualRune) && unicode.IsDigit(nextRune) {
-			repeatRune, err := strconv.Atoi(string(nextRune))
-			if err != nil {
-				return "", err
+		case unicode.IsDigit(r):
+			if !isRune {
+				return "", ErrInvalidString
 			}
-			unpacked.WriteString(strings.Repeat(string(actualRune), repeatRune))
-		} else {
-			unpacked.WriteRune(actualRune)
+			for i := rune(0); i < r-'0'; i++ {
+				unpacked.WriteRune(actualRune)
+			}
+			isRune = false
+		default:
+			if isRune {
+				unpacked.WriteRune(actualRune)
+			}
+			actualRune = r
+			isRune = true
 		}
 	}
+	if isRune {
+		unpacked.WriteRune(actualRune)
+	}
+
 	return unpacked.String(), nil
 }
