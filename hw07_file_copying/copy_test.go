7e7951e0f4aa8d5817f73103d8f350d92842170e
Feride3D <Feride3d@icloud.com>
2022-03-14 01:20:42 +0300

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("copy file to file", func(t *testing.T) {
+		f, _ := os.Create("input.txt")
+		defer os.Remove("input.txt")
+		_, _ = f.WriteString("test")
+
+		err := Copy("input.txt", "input.txt", 0, 0)
+		require.NotNil(t, err)
+	})
+	t.Run("copy /dev/urandom", func(t *testing.T) {
+		err := Copy("/dev/urandom", "/tmp", 0, 0)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+	t.Run("file doesn't exist", func(t *testing.T) {
+		err := Copy("input.txt", "/tmp", 0, 0)
+		require.NotNil(t, err)
+	})
+	t.Run("copy directory", func(t *testing.T) {
+		err := Copy("/tmp", "input.txt", 0, 0)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
 }
